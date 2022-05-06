99dbfe96ae7b5d5456f6c6e2df5b9d78846f19ca
Ajay Ganesh <ajganesh2002@gmail.com>
2022-05-05 21:09:01 -0400

    messing around with workflows

--- Solver/application.py
+++ Solver/application.py
@@ -4,23 +4,23 @@ from solver import Solver
 from trie import serializeTrie
 import json
 
-app = Flask(__name__)
+application = Flask(__name__)
 solver = Solver()
 
-@app.route('/', methods=['GET'])
+@application.route('/', methods=['GET'])
 def hello():
     return "<p>This is an API for an iMessage word hunt solver. Use a POST to the /solve endpoint with a 'board' field formatted as 'abcdefghijklmnop' to return possible words</p>"
 
-@app.route('/solve', methods=['POST'])
+@application.route('/solve', methods=['POST'])
 def solve():
     boardString = request.form['board']
     return json.dumps(solver.solve(boardString), indent = 4)
 
-@app.route('/reserialize', methods=['GET'])
+@application.route('/reserialize', methods=['GET'])
 def serialize():
     serializeTrie()
     solver = Solver()
     return "Done"
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
