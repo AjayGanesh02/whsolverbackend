c63763d23374aef8f7df557a32bdef086a90188e
Ajay Ganesh <ajganesh2002@gmail.com>
2022-05-07 00:46:31 -0400

    trying this

--- Solver/application.py
+++ Solver/application.py
@@ -11,13 +11,10 @@ solver = Solver()
 def hello():
     return "<p>This is an API for an iMessage word hunt solver. Use a POST to the /solve endpoint with a 'board' field formatted as 'abcdefghijklmnop' to return possible words</p>"
 
-@application.route('/solve', methods=['GET','POST'])
+@application.route('/solve')
 def solve():
-    if request.method == 'POST':
-        boardString = request.form['board']
-        return json.dumps(solver.solve(boardString), indent = 4)
-    else:
-        return "<p>Send this endpoint a post req</p>"
+    boardString = request.args.get('board')
+    return json.dumps(solver.solve(boardString), indent = 4)
 
 if __name__ == "__main__":
     application.run(threaded=True, port=5000)
